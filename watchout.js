64cc9651057169e339401b3027e96be3e4712e5d
Hack Reactor Students <ajay.tungare@gmail.com>
2014-06-13 16:55:04 -0700

    Drew enemies as svg elements and updated their location each second

--- watchout.js
+++ watchout.js
@@ -1,2 +1,52 @@
 // start slingin' some d3 here.
 
+var width = parseInt(d3.select("svg").style("width"));
+var height = parseInt(d3.select("svg").style("height"));
+
+var svg = d3.select("svg");
+
+var randPos = function() {
+  var store = [];
+  for(var i = 0; i <= 30; i++) {
+    var x = width * Math.random();
+    var y = height * Math.random();
+    store.push([x, y]);
+  }
+  return store;
+};
+
+var updateEnemies = function(posArr) {
+  // Data Join
+  var enemies = svg.selectAll("circle").data(posArr);
+
+  // Update - NA
+
+  // Enter
+  enemies.enter().append("circle")
+    .attr("cx", function(d) {
+      return d[0];
+    })
+    .attr("cy", function(d){
+      return d[1];
+    })
+    .attr("r", 10)
+    .attr("fill", "black");
+
+  // Update
+  enemies.transition()
+    .attr("cx", function(d) {
+      return d[0];
+    })
+    .attr("cy", function(d){
+      return d[1];
+    });
+
+};
+
+updateEnemies(randPos());
+
+setInterval(function() {
+  var tupArr = randPos();
+  console.log(tupArr);
+  updateEnemies(tupArr);
+}, 1000);
